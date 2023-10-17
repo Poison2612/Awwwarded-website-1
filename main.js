7bf80f501fdb180e20c4e6c5361812d4b766e110
Sahil Solankey <poison@Sahils-MacBook-Air.local>
2023-10-17 10:44:11 +0530

    added page 2 working

--- main.js
+++ main.js
@@ -1,62 +1,51 @@
-
-function playVideo(){
-var videoCon = document.querySelector(".video-container");
-var playCrsr = document.querySelector("#play");
-
-videoCon.addEventListener("mouseenter", function(){
-    gsap.to(playCrsr, {opacity: 1, scale: 1});
+const scroll = new LocomotiveScroll({
+  el: document.querySelector('#main'),
+  smooth: true
 });
- videoCon.addEventListener("mouseleave", function(){
-    gsap.to(playCrsr, {opacity: 0, scale: 0});
-});
+var play = document.querySelector("#play");
+var videocon = document.querySelector('#video-container');
 
-videoCon.addEventListener("mousemove", function(dets){
-    console.log(dets.clientX, dets.clientY);
+function playVideo() {
+  videocon.addEventListener("mouseenter", function(){
+    gsap.to(play, {
+      duration: 0.5, 
+      scale: 1, 
+      opacity: 1});
+  });
+  videocon.addEventListener("mouseleave", function(){
+    gsap.to(play, {
+      duration: 0.5, 
+      scale: 0, 
+      opacity: 0
+    });
+  
+  });
+  
+  videocon.addEventListener("mousemove", function(dets){
+    gsap.to(play, {
     
-    gsap.to(playCrsr, {top: dets.clientY-50, left: dets.clientX-50});
-
-});
+      left: dets.x - 30, 
+      top: dets.y -30
+    });
+  
+  });
 };
+
 playVideo();
 
-function loadinganimation() {
-    gsap.from("#page1 h1", {
-      y: 100,
-      opacity: 0,
-      delay: 0.5,
-      duration: 0.9,
-      stagger: 0.3,
-    });
-    gsap.from("#page1 #video-container", {
-      scale: 0.9,
-      opacity: 0,
-      delay: 1.3,
-      duration: 0.5,
-    });
-  }
-
-  loadinganimation();
-
-  function loadinganimationbytextillate(){
-    gsap.from("#page1 h1", {
-        opacity: 0,
-        delay: 1,
-        duration: 0.9,
-        stagger: 0.3,
-        onStart: function () {
-            $("#page1 h1").textillate({in: {effect: "fadeInUp", sync: true}})
-        },
-        
-      });
-    
-      gsap.from("#page1 #video-container", {
-        opacity:0,
-        delay: 1.3,
-        duration: 0.5,
-        onStart: function () {
-            $("#page1 #video-container").textillate({in: {effect: "fadeInUp", sync: true}})
-        }});
-            
-
-  }
-
+function loadingAnimaiton() {
+  gsap.from("#page1 h1", {
+    y: 100,
+    opacity: 0,
+    duration: 0.9,
+    delay: 0.5,
+    stagger: 0.3,
+  });
+  gsap.from("#video-container", {
+    scale: 0.9,
+    opacity: 0,
+    duration: 0.3,
+    delay: 1.5,
+  });
+};
+loadingAnimaiton();
